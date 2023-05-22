5ae554c97d468aa58fe46cfd8bf7d3382eb7ecf3
mar1cruz <ddovidovich@list.ru>
2023-05-22 20:48:35 +0300

    added

---
+++ src/js/index.js
@@ -0,0 +1,21 @@
+// меню бургер
+
+const iconMenu = document.querySelector(".menu-icon");
+const headerNavigation = document.querySelector(".header-navigation");
+const headerLink = document.querySelector(".nav__links");
+const burgerClose = document.querySelector(".burger-close");
+if (iconMenu) {
+  iconMenu.addEventListener("click", function (e) {
+    document.body.classList.toggle("_lock");
+    iconMenu.classList.toggle("_active");
+    headerNavigation.classList.toggle("_active");
+  });
+}
+
+if (headerLink) {
+  burgerClose.addEventListener("click", function (e) {
+    document.body.classList.toggle("_lock");
+    iconMenu.classList.toggle("_active");
+    headerNavigation.classList.toggle("_active");
+  });
+}
